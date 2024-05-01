1881b9afb2975775675cb480d504237053f5fc62
amodhnepal <nepalamodh8@gmail.com>
2024-05-01 09:12:49 +0545

    Did 3 basic assignments

---
+++ starter/assignment.js
@@ -0,0 +1,35 @@
+
+
+// Assignment 1
+let Country="Nepal";
+let Continent="Asia";
+let pop="30.55";
+
+console.log(Country);
+console.log(Continent);
+console.log(pop);
+
+// Assignment 2
+Language ='Nepali';
+const Desh= 'Nepal';
+const continent='Asia';
+Language='Newari';
+console.log(Language);
+
+// Assignemnt 3
+let country='Nepal';
+let population ='30.55';
+// If country split into equal half and divide population eqully the,
+let newPopulation=population/2;
+console.log(newPopulation);
+population=31.55;
+console.log(population);
+ let finLandPopulation=6;
+ let highPopulation=population>finLandPopulation;
+ console.log(highPopulation);
+ let avgPopulation=33;
+ let leastPopulation=avgPopulation>population;
+
+ const description=country+'is in' +continent+'and its'+population+'million people speak'+ Language;
+ console.log(description);
+ 
---
+++ starter/script.js
@@ -0,0 +1,29 @@
+/*
+
+let js ="Amazing";
+console.log(40+8+16-19);
+
+let firstName = "Amodh"; //Declaring a variable
+console.log(firstName);
+
+javascriptIsFun ='YES';
+console.log(typeof javascriptIsFun);
+let year;
+console.log(year);
+console.log(typeof year); 
+year=2024;
+console.log(typeof year);
+console.log(typeof null);
+
+*/
+
+const now=2030;
+const ageAmodh=now-2002;
+const ageUser=now-2015;
+console.log(ageAmodh,ageUser);
+
+console.log(ageAmodh*2,ageUser/10,2**3);
+// 2**3 means 2 to the power of 3 = 2*2*2
+const firstName='Amodh';
+const lastName='Nepal';
+console.log(firstName+' '+lastName);
